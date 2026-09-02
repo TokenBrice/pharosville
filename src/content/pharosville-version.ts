28a8969232d9c6cd4fbb96e6ddcee431ee8aefa8
TokenBrice <me@tokenbrice.com>
2026-09-02 18:56:16 +0200

    chore(release): prepare v0.9.0
    (cherry picked from commit 2347bec644563ae26337055d01290642add727e0)

--- src/content/pharosville-version.ts
+++ src/content/pharosville-version.ts
@@ -16,6 +16,7 @@ export const PHAROSVILLE_RELEASE_VERSIONS = {
   roomierHarbor: "v0.7.1",
   quietAnchorage: "v0.7.2",
   gardenOfLight: "v0.8.0",
+  sevenWaterGarden: "v0.9.0",
 } as const;
 
-export const PHAROSVILLE_LATEST_VERSION = PHAROSVILLE_RELEASE_VERSIONS.gardenOfLight;
+export const PHAROSVILLE_LATEST_VERSION = PHAROSVILLE_RELEASE_VERSIONS.sevenWaterGarden;
